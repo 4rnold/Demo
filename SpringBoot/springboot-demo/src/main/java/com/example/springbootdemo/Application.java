5168c3b17a6c87ceac7bc1b8a4c09e4123ceaa4a
4rnold <arnold1989@sina.com>
2020-05-16 15:04:27 +0800

    something

--- SpringBoot/springboot-demo/src/main/java/com/example/springbootdemo/Application.java
+++ SpringBoot/springboot-demo/src/main/java/com/example/springbootdemo/Application.java
@@ -5,8 +5,13 @@ import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {RedisAutoConfiguration.class, RedissonAutoConfiguration.class, MybatisPlusAutoConfiguration.class})
+@SpringBootApplication(exclude= {
+        RedisAutoConfiguration.class,
+        RedissonAutoConfiguration.class,
+        MybatisPlusAutoConfiguration.class,
+        DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
