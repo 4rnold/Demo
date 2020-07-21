4068e14875df9d4108086a1505faeabdc264d441
4rnold <arnold1989@sina.com>
2020-07-21 10:22:31 +0800

    RabbitMQ Demo

--- SpringBoot/spring-rabbitmq-demo/src/test/java/com/example/springrabbitmqdemo/SpringRabbitmqDemoApplicationTests.java
+++ SpringBoot/spring-rabbitmq-demo/src/test/java/com/example/springrabbitmqdemo/SpringRabbitmqDemoApplicationTests.java
@@ -1,8 +1,17 @@
 package com.example.springrabbitmqdemo;
 
+import com.example.springrabbitmqdemo.entity.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class SpringRabbitmqDemoApplicationTests {
 
@@ -10,4 +19,28 @@ class SpringRabbitmqDemoApplicationTests {
     void contextLoads() {
     }
 
+//    @Autowired
+//    private RabbitAdmin rabbitAdmin;
+//
+//
+//    @Test
+//    public void test() {
+//        rabbitAdmin.declareExchange(new DirectExchange("test.direct",false,false));
+//    }
+
+    @Autowired
+    RabbitSender sender;
+
+    @Test
+    public void test() {
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("number", "123");
+        sender.send("message", properties);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        Order order = new Order("001", "第一个订单");
+        sender.sendOrder(order);
+    }
 }
